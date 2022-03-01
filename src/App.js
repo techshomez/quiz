205b2fadacb07e3fa7f25f834b9fa86915b49af9
techshomez <danieltopeg@gmail.com>
2022-03-01 16:51:23 +0100

    quiz instructions page update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from './components/Home';
+import QuizInstruction from "./components/quiz/QuizInstructions";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/play/instructions" element={<QuizInstruction />} />
       </Routes>
     </BrowserRouter>
   )
--- src/components/quiz/QuizInstructions.js
+++ src/components/quiz/QuizInstructions.js
@@ -1,5 +1,28 @@
 import React, { Component, Fragment} from 'react';
+import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 
 const QuizInstruction = () => (
-    <h1>Quiz Instruction Page</h1>
+    <Fragment>
+        <Helmet><title>Quiz | Instruction</title></Helmet>
+        <div className="instructions container">
+            <h1>How to take Quiz</h1>
+            <p>Ensure you read this guide before you take the quiz</p>
+            <ul className='browser-default' id='main-list'>
+                <li>The Quiz has a duration of 15 minutes and ends as soon as your time elapses.</li>
+                <li>Each Quiz consists of 15 questions</li>
+                <li>Each Quiz has 2 lifelines</li>
+                <li>
+                    Choosing a 50-50 lifeline by clicking the icon by top left of the page
+                    <span className='mdi mdi-set-center mdi-24px lifeline-icon'></span>
+                </li>
+            </ul>
+            <div>
+                <span className="left"><Link to="/">No take me back</Link></span>
+                <span className="right"><Link to="/play">Okay, Let's do this!</Link></span>
+            </div>
+        </div>
+    </Fragment>
 );
+
+export default QuizInstruction;
