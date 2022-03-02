c1fdddd03c40013ce5eda9280047e389de8dd716
techshomez <danieltopeg@gmail.com>
2022-03-02 16:09:28 +0100

    quiz question behavior updated

--- src/components/quiz/Take.js
+++ src/components/quiz/Take.js
@@ -1,18 +1,94 @@
 import React, { Component, Fragment } from 'react';
 import Helmet from 'react-helmet';
+import M from 'materialize-css';
+
+import questions from '../../questions.json';
+import isEmpty from '../../utills/is-empty';
 
 class Take extends Component {
-    /*constructor (props){
+    constructor (props){
         super(props);
+        this.state = {
+            questions,
+            currentQuestion: {},
+            nextQuestion: {},
+            previousQuestion: {},
+            answer: '',
+            numberofQuestions: 0,
+            numberofAnsweredQuestion: 0,
+            currentQuestionIndex: 0,
+            score: 0,
+            correntAnswers: 0,
+            wrongAnswers: 0,
+            fiftyFifty: 2,
+            usedFiftyFIfty: false,
+            time: {}
+        };
         
-    }*/
-
-    increaseCount = () =>{
-        this.setState({
-            counter: 5
-        });
+        
     }
+
+    componentDidMount () {
+        const { questions, currentQuestion, nextQuestion, previousQuestion } = this.state;
+        this.displayQuestions(questions, currentQuestion, nextQuestion, previousQuestion);
+    }
+
+    displayQuestions = (questions = this.state.questions, currentQuestion, nextQuestion, previousQuestion) => {
+        let { currentQuestionIndex } = this.state;
+        if (!isEmpty(this.state.questions)) {
+            questions = this.state.questions;
+            currentQuestion = questions[currentQuestionIndex];
+            nextQuestion = questions[currentQuestionIndex  + 1];
+            previousQuestion = questions[currentQuestionIndex - 1];
+            const answer = currentQuestion.answer;
+            this.setState({
+                currentQuestion,
+                nextQuestion,
+                previousQuestion,
+                answer
+            });
+        }
+    };
+
+    handleOptionClick = (e) => {
+        if(e.target.innerHTML.toLowerCase() === this.state.answer.toLowerCase()) {
+            this.correctAnswer();
+        } else {
+            this.wrongAnswer();
+        }
+    }
+
+    correctAnswer = () => {
+        M.toast({
+            html: 'Answer Correct!',
+            classes: 'toast-valid',
+            displayLength: 1500
+        });
+        this.setState(prevState =>({
+            score: prevState.score + 1,
+            correctAnswers: prevState.correctAnswers + 1,
+            currentQuestionIndex: prevState.currentQuestionIndex,
+            numberofAnsweredQuestion: prevState.numberofAnsweredQuestion + 1
+        }));
+    }
+
+    wrongAnswer = () => {
+        navigator.vibrate(1000);
+        M.toast({
+            html: 'Wrong Answer!',
+            classes: 'toast-invalid',
+            displayLength: 1500
+        });
+        this.setState(prevState =>({
+            wrongAnswers: prevState.wrongAnswers + 1,
+            currentQuestionIndex: prevState.currentQuestionIndex + 1,
+            numberofAnsweredQuestion: prevState.numberofAnsweredQuestion
+        }));
+    }
+
     render () {
+        const { currentQuestion } = this.state;
+
         return (
             <Fragment>
                 <Helmet><title>Quiz | Questions</title></Helmet>
@@ -32,14 +108,14 @@ class Take extends Component {
                             <span className='right'>2:15<span className='mdi mdi-clock-outline mdi-24px'></span></span>
                         </p>
                     </div>
-                    <h5>Google was founded in what years?</h5>
+                    <h5>{currentQuestion.question}</h5>
                     <div className="options-container">
-                        <div className="option">1997</div>
-                        <div className="option">1998</div>
+                        <p onClick={this.handleOptionClick} className="option">{currentQuestion.optionA}</p>
+                        <p onClick={this.handleOptionClick} className="option">{currentQuestion.optionB}</p>
                     </div>
                     <div className="options-container">
-                    <div className="option">1999</div>
-                    <div className="option">2000</div>
+                        <p onClick={this.handleOptionClick} className="option">{currentQuestion.optionC}</p>
+                        <p onClick={this.handleOptionClick} className="option">{currentQuestion.optionD}</p>
                     </div>
                     <div className="button-container">
                         <button>Previous</button>
